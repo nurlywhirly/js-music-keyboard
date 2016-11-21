37428a333f0ed521e5a18a4dc5ce3e0ddfc72285
Nicole Url <nicole.url.4@gmail.com>
2016-11-21 15:22:30 -0800

    Got semi-hard-coded notes working

--- noise.js
+++ noise.js
@@ -1,3 +1,14 @@
 $(document).ready( function() {
-  // your code here
+  $('.instrument').on('click', 'button', function(event) {
+    var note = $(this).html();
+    playSound(note);
+  })
 });
+
+var playSound = function(note) {
+  var id = "#" + note + "Audio",
+      file = "media/" + note + "_note.mp3",
+      audio = new Audio(file);
+
+  audio.play();
+};
