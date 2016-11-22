30f33ebfdd9d2e8f3e1a2477128f1ad9ca9326a0
Nicole Url <nicole.url.4@gmail.com>
2016-11-21 16:01:37 -0800

    Got keys on keyboard to work with still hard-coded thingie

--- noise.js
+++ noise.js
@@ -1,12 +1,18 @@
 $(document).ready( function() {
-  $('.instrument').on('click', 'button', function(event) {
+  $('.instrument').on('click', 'button', function() {
     var note = $(this).html();
     playSound(note);
-  })
+  });
+
+  $(window).keydown(function(event) {
+    var note = event.key;
+    playSound(note);
+  });
+
 });
 
 var playSound = function(note) {
-  var id = "#" + note + "Audio",
+  var //id = "#" + note + "Audio", // I guess not needed?
       file = "media/" + note + "_note.mp3",
       audio = new Audio(file);
 
